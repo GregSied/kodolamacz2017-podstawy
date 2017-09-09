a395a0fa63a3af1c963ffdc806975246976ab45d
Bartłomiej Tartanus <bartektartanus@gmail.com>
2017-09-09 14:52:54 +0200

    dodany main w pliku nowaklasa

--- src/pl/kodolamacz/podstawy/NowaKlasa.java
+++ src/pl/kodolamacz/podstawy/NowaKlasa.java
@@ -1,4 +1,8 @@
 package pl.kodolamacz.podstawy;
 
 public class NowaKlasa {
+    public static void main(String[] args) {
+        System.out.println(1 + 2);
+        System.out.println(16 < 88 & 2 * 5 == 10);
+    }
 }
